286ef80117a6076124adbcf485f992e6f11df10f
Dolloby <kevintorval@outlook.com>
2024-12-10 19:29:21 -0500

    test env2

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,13 +4,13 @@ const { Sequelize } = require('sequelize');
 
 //Configuraci&oacute;n de conexi&oacute;n a la base de datos
 const sequelize = new Sequelize(
-        process.env.REACT_APP_BACKEND_URL.DB_NAME,
-        process.env.REACT_APP_BACKEND_URL.DB_USER,
-        process.env.REACT_APP_BACKEND_URL.DB_PASS,
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASS,
     {
-    host: process.env.REACT_APP_BACKEND_URL.DB_HOST, // Cambia el host si tu servidor de base de datos est&aacute; en otro lado
-    dialect:process.env.REACT_APP_BACKEND_URL.DB_DIALECT, // Cambia el
-    port: process.env.REACT_APP_BACKEND_URL.DB_PORT, //
+    host: process.env.DB_HOST, // Cambia el host si tu servidor de base de datos est&aacute; en otro lado
+    dialect:process.env.DB_DIALECT, // Cambia el
+    port: process.env.DB_PORT, //
     logging: false, // Desactiva el log de consultas si prefieres que no se vean en la consola
 });
 
